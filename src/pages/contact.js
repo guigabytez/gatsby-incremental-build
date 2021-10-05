277ced43c414d20b041c0605479a31ec92b5aa94
guigabytez <ahg0406@gmail.com>
2021-10-05 16:21:08 +0800

    added form on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const Contact = () => {
-    return (
-        <>
-            <Layout pageHeading="Contact Us" pageTitle="Contact"/>
-        </>
+    return ( 
+        <Layout pageHeading="Contact Us" pageTitle="Contact">
+            <form>
+                <input placeholder="Email"/>
+                <input placeholder="Subject"/>
+                <textarea placeholder="Message"/>
+            </form>
+        </Layout>
     )
 }
 
